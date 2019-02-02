16c9b5feb9865edd5dc88f71b45b196c9845394f
DinnerBeef <Dinnerbeef18@gmail.com>
2019-02-02 12:46:47 -0600

    Got Fancy Things To Work

--- src/main/java/beef/faceblock/Blocks/DinnerBeef.java
+++ src/main/java/beef/faceblock/Blocks/DinnerBeef.java
@@ -4,7 +4,12 @@ import beef.faceblock.Faceblock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class DinnerBeef extends Block {
 
@@ -18,6 +23,8 @@ public class DinnerBeef extends Block {
         setSoundType(SoundType.STONE);
         setCreativeTab(Faceblock.FBTabs);
     }
+
+    @SideOnly(Side.CLIENT) public void initModel() { ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory")); }
 }
 
 
--- src/main/java/beef/faceblock/Blocks/LatvianModder.java
+++ src/main/java/beef/faceblock/Blocks/LatvianModder.java
@@ -4,7 +4,12 @@ import beef.faceblock.Faceblock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class LatvianModder extends Block {
 
@@ -18,4 +23,6 @@ public class LatvianModder extends Block {
         setSoundType(SoundType.STONE);
         setCreativeTab(Faceblock.FBTabs);
     }
+
+    @SideOnly(Side.CLIENT) public void initModel() { ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory")); }
 }
